848b2e017e8dbd97203002c0d60ce0e6a8211992
hasith-schoolgrove <hasith.lakshan@schoolgrove.com>
2022-02-17 12:36:28 +0530

    Add comments, update gitignore and Project instructions :Sparkles:

--- cat.js
+++ cat.js
@@ -1,63 +1,58 @@
-let { writeFile } = require("fs")
-let { join } = require("path")
-let blend = require("@mapbox/blend")
-let argv = require("minimist")(process.argv.slice(2))
+let { writeFile } = require('fs');
+let { join } = require('path');
+let blend = require('@mapbox/blend');
+let argv = require('minimist')(process.argv.slice(2));
 
-const { getCat } = require("./services/CatService")
+const { getCat } = require('./services/CatService');
 
-let {
-  greeting = "Hello",
-  who = "You",
-  width = 400,
-  height = 500,
-  color = "Pink",
-  size = 100,
-} = argv
+let { greeting = 'Hello', who = 'You', width = 400, height = 500, color = 'Pink', size = 100 } = argv;
 
-const createImage = async () => {
+/**
+ * This funtion will fetch two images that need to bind together
+ * @returns null
+ */
+const fetchImages = async () => {
   try {
     const firstResponse = await getCat(
-      "https://cataas.com/cat/says/" +
+      'https://cataas.com/cat/says/' +
         greeting +
-        "?width=" +
+        '?width=' +
         width +
-        "&height=" +
+        '&height=' +
         height +
-        "&color" +
+        '&color' +
         color +
-        "&s=" +
+        '&s=' +
         size
-    )
+    );
     console.log('Received response with status:' + firstResponse.status);
     const secondResponse = await getCat(
-      "https://cataas.com/cat/says/" +
-        who +
-        "?width=" +
-        width +
-        "&height=" +
-        height +
-        "&color" +
-        color +
-        "&s=" +
-        size
-    )
+      'https://cataas.com/cat/says/' + who + '?width=' + width + '&height=' + height + '&color' + color + '&s=' + size
+    );
     console.log('Received response with status:' + secondResponse.status);
-    mergeImages(firstResponse.data, secondResponse.data)
+    return mergeImages(firstResponse.data, secondResponse.data);
   } catch (error) {
-    console.log(error)
-    return
+    console.log(error);
+    return;
   }
-}
+};
+
+/**
+ * This funtion will merge two images in to one
+ * @param {string} firstImage
+ * @param {string} secondImage
+ * @returns
+ */
 const mergeImages = (firstImage, secondImage) =>
   blend(
     [
       {
-        buffer: new Buffer.from(firstImage, "binary"),
+        buffer: new Buffer.from(firstImage, 'binary'),
         x: 0,
         y: 0,
       },
       {
-        buffer: new Buffer.from(secondImage, "binary"),
+        buffer: new Buffer.from(secondImage, 'binary'),
         x: width,
         y: 0,
       },
@@ -65,18 +60,21 @@ const mergeImages = (firstImage, secondImage) =>
     {
       width: width * 2,
       height: height,
-      format: "jpeg",
+      format: 'jpeg',
     },
     (err, data) => {
-      const fileOut = join(process.cwd(), `/cat-card.jpg`)
-      writeFile(fileOut, data, "binary", (err) => {
+      const fileOut = join(process.cwd(), `/cat-card.jpg`);
+      writeFile(fileOut, data, 'binary', (err) => {
         if (err) {
-          console.log(err)
-          return
+          console.log(err);
+          return;
         }
-        console.log("The file was saved!")
-      })
+        console.log('The file was saved!');
+      });
     }
-  )
+  );
 
-createImage()
+/**
+ * This function will start processing the images
+ */
+fetchImages();
